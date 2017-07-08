cd60d618c8d53547c2612ef3275231620b82bc83
Maxitosh <maxitoshs@gmail.com>
2017-07-08 16:23:26 +0300

    Added dataGridViewer

--- daily80/frmMain.Designer.cs
+++ daily80/frmMain.Designer.cs
@@ -28,8 +28,18 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.components = new System.ComponentModel.Container();
             this.btnStartCreateAccs = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Battle_TAG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lvl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.daily80 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.timerData = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStartCreateAccs
@@ -52,15 +62,76 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Battle_TAG,
+            this.Lvl,
+            this.email,
+            this.daily80});
+            this.dataGridView.Location = new System.Drawing.Point(310, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(585, 445);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Battle_TAG
+            // 
+            this.Battle_TAG.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Battle_TAG.HeaderText = "Battle_TAG";
+            this.Battle_TAG.Name = "Battle_TAG";
+            this.Battle_TAG.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Lvl
+            // 
+            this.Lvl.HeaderText = "Lvl";
+            this.Lvl.Name = "Lvl";
+            this.Lvl.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // email
+            // 
+            this.email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            this.email.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // daily80
+            // 
+            this.daily80.HeaderText = "daily80";
+            this.daily80.Name = "daily80";
+            this.daily80.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(188, 404);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(105, 53);
+            this.btnLoadData.TabIndex = 3;
+            this.btnLoadData.Text = "Load DATA";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // timerData
+            // 
+            this.timerData.Interval = 5000;
+            this.timerData.Tick += new System.EventHandler(this.timerData_Tick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(667, 469);
+            this.ClientSize = new System.Drawing.Size(907, 469);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnStartCreateAccs);
             this.Name = "frmMain";
             this.Text = "QuestMiner";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
 		}
@@ -69,6 +140,14 @@
 
         private System.Windows.Forms.Button btnStartCreateAccs;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Battle_TAG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lvl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn daily80;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Timer timerData;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- daily80/frmMain.cs
+++ daily80/frmMain.cs
@@ -27,7 +27,7 @@ namespace daily80
 		{
 			InitializeComponent();
 			AllocConsole();
-			Console.WriteLine(Reflection.GetBattleTag().Name + "#" + Reflection.GetBattleTag().Number);
+			//Console.WriteLine(Reflection.GetBattleTag().Name + "#" + Reflection.GetBattleTag().Number);
 		}
 
 	    private void btnStartCreateAccs_Click(object sender, EventArgs e)
@@ -71,6 +71,51 @@ namespace daily80
             {
                 Console.WriteLine(exception);
                 throw;
+            }
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+
+            Console.WriteLine("File opened!");
+        }
+
+        private void timerData_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Saving data!");
+            try
+            {
+                DataSet ds = new DataSet(); // создаем пока что пустой кэш данных
+                DataTable dt = new DataTable(); // создаем пока что пустую таблицу данных
+                dt.TableName = "Account"; // название таблицы
+                dt.Columns.Add("Battle_TAG"); // название колонок
+                dt.Columns.Add("Lvl");
+                dt.Columns.Add("Email");
+                dt.Columns.Add("daily80");
+                ds.Tables.Add(dt); //в ds создается таблица, с названием и колонками, созданными выше
+
+                foreach (DataGridViewRow r in dataGridView.Rows) // пока в dataGridView1 есть строки
+                {
+                    DataRow row = ds.Tables["Account"].NewRow(); // создаем новую строку в таблице, занесенной в ds
+                    row["Battle_TAG"] = r.Cells[0].Value;  //в столбец этой строки заносим данные из первого столбца dataGridView
+                    row["Lvl"] = r.Cells[1].Value; 
+                    row["Email"] = r.Cells[2].Value;
+                    row["daily80"] = r.Cells[3].Value;
+                    ds.Tables["Account"].Rows.Add(row); //добавление всей этой строки в таблицу ds.
+                }
+                ds.WriteXml("accounts.xml");
+                Console.WriteLine("XML file was successfully saved.");
+            }
+            catch
+            {
+                Console.WriteLine("Cant save XML file.");
             }
         }
     }
